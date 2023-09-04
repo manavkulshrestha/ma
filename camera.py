764da387d8b7e7b743a1f139bf11f04aadc91461
Eduardo AG <edag4000@gmail.com>
2023-09-04 12:38:01 -0400

    Camera sensor rgb with returning pixel value including distance

--- camera.py
+++ camera.py
@@ -4,18 +4,23 @@ import mujoco
 import mujoco.viewer
 import mujoco.renderer
 import mujoco.glfw
-
 import matplotlib.pyplot as plt
 import numpy as np
 import sensors
+
+
 
 # Debug flag
 debug = True
 test_name = "camera_depth"
 
+
+
 # Creating OpenGL context
 ctx = mujoco.GLContext(1200, 900)
 ctx.make_current()
+
+
 
 # Creating xml mujoco object with a camera
 xml = """
@@ -27,7 +32,7 @@ xml = """
         <body name="robot" pos="0 0 1">
             <joint type="free"/>
             <geom type="sphere" size="0.1" rgba="0 0 1 1"/>
-            <camera name="camera" pos="0 0 1" zaxis="0 0 2" mode="fixed" fovy="70" />
+            <camera name="camera" pos="0 0 1" zaxis="0 0 2" mode="fixed" fovy="60" />
         </body>
     </worldbody>
     <visual>
@@ -36,68 +41,54 @@ xml = """
 </mujoco>
 """
 
+
+
 # Creating a model from the xml
 m = mujoco.MjModel.from_xml_string(xml)
 d = mujoco.MjData(m)
-r = mujoco.Renderer(m, 900, 1200)
-r_depth = mujoco.Renderer(m, 480, 640)
+r_rgb = mujoco.Renderer(m, 900, 1200)
+r_depth = mujoco.Renderer(m, 900, 1200)
 r_depth.enable_depth_rendering()
 s = sensors.Sensors()
 
-# Variable creation for MjvCamer (TODO: Learn how to use this)
-# cam = mujoco.MjvCamera()
-# opt = mujoco.MjvOption()
-# scn = mujoco.MjvScene()
-# con = mujoco.MjrContext()
+
 
 # Variables for pixel debugging
 steps = 0
 img_n = 0
 
-def transform_depth(depth):
-
-  # Shift nearest values to the origin.
-  depth -= depth.min()
-  # Scale by 2 mean distances of near rays.
-  depth /= 2*depth[depth <= 1].mean()
-  # Scale to [0, 255]
-  pixels = 255*np.clip(depth, 0, 1)
-
-  return pixels
 
 # Launching the viewer
 with mujoco.viewer.launch_passive(m, d) as viewer:
-  # Setting the camera
-  print(type(m.cam("camera")))
-  print(type(viewer.cam))
 
   # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
 
-
   while viewer.is_running() and time.time() - start < 30:
+
     step_start = time.time()
+
     # mj_step can be replaced with code that also evaluates
     # a policy and applies a control signal before stepping the physics.
     mujoco.mj_step(m, d)
 
-
     # Saving pixels to make sure sensor is working correctly
-    r_depth.update_scene(d, 0)
+    # r_rgb.update_scene(d, 0)
+    # r_depth.update_scene(d, 0)
     
-    if steps > 10 and debug:
-      depth = r_depth.render()
-      print(depth)
-      print(depth.min())
-      print(depth.max())
-      pixels = transform_depth(depth)
-      plt.imsave(f"./{test_name}_{img_n}.png", pixels.astype(np.uint8), cmap="gray")
-      steps = 0
-      img_n += 1
-    steps += 1
-      
-    # pixels = s.get_rgb_image_matrices(m, d, r)
-    # print(pixels[0])
+    # if steps > 10 and debug:
+    #   depth = r_depth.render()
+    #   print(depth)
+    #   print(depth.min())
+    #   print(depth.max())
+
+    #   steps = 0
+    #   img_n += 1
+    # steps += 1
+    
+    # Reading pixels from the cameras
+    pixels = s.get_rgbd_image_matrices(m, d, r_rgb, r_depth)
+    print(pixels[0])
 
     # Example modification of a viewer option: toggle contact points every two seconds.
     with viewer.lock():
--- sensors.py
+++ sensors.py
@@ -6,10 +6,11 @@ class Sensors:
     def __init__(self) -> None:
         pass
     
-    def get_rgb_image_matrices(self, m:mujoco.MjModel, 
-                                     d:mujoco.MjData,
-                                     r:mujoco.Renderer,
-                                     cameras=None|list[str]|list[int]) -> dict[any, np.ndarray]:
+    def get_rgb_image_matrices(self,
+                               m:mujoco.MjModel,
+                               d:mujoco.MjData,
+                               r:mujoco.Renderer,
+                               cameras=None|list[str]|list[int]) -> dict[any, np.ndarray]:
         """
         Returns dictionary of RGB image matrices for each camera in the model.
 
@@ -35,3 +36,47 @@ class Sensors:
                 result[cam] = r.render()
                 
         return result
+    
+    def get_rgbd_image_matrices(self, m:mujoco.MjModel, 
+                                     d:mujoco.MjData,
+                                     r_rgb:mujoco.Renderer,
+                                     r_depth:mujoco.Renderer,
+                                     cameras=None|list[str]|list[int]) -> dict[any, np.ndarray]:
+        """
+        Returns dictionary of RGBD image matrices for each camera in the model.
+
+        Parameters
+        ----------
+        m : The mujoco model.
+        d : The mujoco data.
+        r_rgb : The mujoco renderer for RGB images.
+        r_depth : The mujoco renderer for depth images.
+        cameras : The list of cameras to render. If None, all cameras are rendered. The default is None.
+        """
+
+        result = {}
+
+        if type(cameras) == list:
+            cams = cameras
+        else:
+            cams = range(m.cam_user.shape[0])
+
+        for cam in cams:
+                r_rgb.update_scene(d, cam)
+                r_depth.update_scene(d, cam)
+                # Can add out = np.zeros((m.vis.map[cam].height, m.vis.map[cam].width, 3), 
+                # dtype=np.uint8) to render() to save memory
+                try:
+                    # Add a dimension to the depth image to make it 3D
+                    depth = np.expand_dims(r_depth.render(), axis=2)
+
+                    # TODO: Limit the depth values to be between 0.3 and 4.0
+                    # depth = np.where(depth >= 0.3 or 4.0 <= depth, depth.all(), 0)
+                    # Join the RGB and depth images
+                    rgb = r_rgb.render()
+                    result[cam] = np.append(rgb, depth, axis=2)
+                except:
+                    raise Exception("Error joining depth and rgb. Reading have different sizes.")
+                
+        return result
+
