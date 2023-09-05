8d61d5a8e6ce706dd6cf8efd8e96f5bef8d3f1ff
Eduardo AG <edag4000@gmail.com>
2023-09-05 17:11:38 -0400

    Added funtion to save video stream from all present cameras / Better comments

--- camera.py
+++ camera.py
@@ -7,22 +7,21 @@ import mujoco.glfw
 import matplotlib.pyplot as plt
 import numpy as np
 import sensors
+import mediapy as media
 
 
 
-# Debug flag
-debug = True
+# ------------ Debug flag ----------------
+debug = False
 test_name = "camera_depth"
 
 
-
-# Creating OpenGL context
+# ------- Creating OpenGL context --------
 ctx = mujoco.GLContext(1200, 900)
 ctx.make_current()
 
 
-
-# Creating xml mujoco object with a camera
+# ------------ XML DEFINITION ------------
 xml = """
 <mujoco>
     <worldbody>
@@ -42,8 +41,7 @@ xml = """
 """
 
 
-
-# Creating a model from the xml
+# ------- MODEL AND DATA CREATION ---------
 m = mujoco.MjModel.from_xml_string(xml)
 d = mujoco.MjData(m)
 r_rgb = mujoco.Renderer(m, 900, 1200)
@@ -52,57 +50,78 @@ r_depth.enable_depth_rendering()
 s = sensors.Sensors()
 
 
-
-# Variables for pixel debugging
+# ----- Variables for pixel debugging ------
 steps = 0
 img_n = 0
+
+def save_image(name, pixels, renderer):
+  plt.imsave(name, pixels)
+
+
+# ----- Variables for video creation -------
+n_cameras = m.cam_user.shape[0]
+videos = [[] for _ in range(n_cameras)]
 
 
 # Launching the viewer
 with mujoco.viewer.launch_passive(m, d) as viewer:
 
+
   # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
 
-  while viewer.is_running() and time.time() - start < 30:
+
+  while viewer.is_running() and time.time() - start < 10:
 
     step_start = time.time()
+
 
     # mj_step can be replaced with code that also evaluates
     # a policy and applies a control signal before stepping the physics.
     mujoco.mj_step(m, d)
 
-    # Saving pixels to make sure sensor is working correctly
-    # r_rgb.update_scene(d, 0)
-    # r_depth.update_scene(d, 0)
-    
-    # if steps > 10 and debug:
-    #   depth = r_depth.render()
-    #   print(depth)
-    #   print(depth.min())
-    #   print(depth.max())
 
-    #   steps = 0
-    #   img_n += 1
-    # steps += 1
+    # ------ Debug Code for Image Saving ------
+    if debug:
+      # Saving pixels to make sure sensor is working correctly
+      r_rgb.update_scene(d)
+      r_depth.update_scene(d)
+
+      # Saving pixels to make sure sensor is working correctly
+      if steps > 10:
+        save_image("rgb_" + str(img_n) + ".png", r_rgb.render())
+        steps = 0
+        img_n += 1
+      steps += 1
     
-    # Reading pixels from the cameras
+
+    # ------ Debug Code for Video Saving ------
     pixels = s.get_rgbd_image_matrices(m, d, r_rgb, r_depth)
-    print(pixels[0])
+    for i in range(n_cameras):
+      videos[i].append(pixels[i][:, :, :3])
+
 
     # Example modification of a viewer option: toggle contact points every two seconds.
     with viewer.lock():
       viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
 
+
     # Pick up changes to the physics state, apply perturbations, update options from GUI.
     viewer.sync()
+
 
     # Rudimentary time keeping, will drift relative to wall clock.
     time_until_next_step = m.opt.timestep - (time.time() - step_start)
     if time_until_next_step > 0:
       time.sleep(time_until_next_step)
 
+
     time.sleep(.01)
+
+# Saving the videos
+for i in range(n_cameras):
+  media.write_video("video_cam_" + str(i) + ".mp4", videos[i], fps=30)
+
 
 # Freeing the context
 ctx.free()
