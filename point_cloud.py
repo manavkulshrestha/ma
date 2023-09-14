Correct match to world coordinates. PENDING correct rotation

point_cloud.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 
@@ -7,7 +8,7 @@ class PointCloud:
         self.shape = shape
 
 
-    
+
     # A custom function to calculate
     # probability distribution function
     def pdf(self, size):
@@ -31,25 +32,30 @@ class PointCloud:
         camera_quat : The quaternion of the camera in the world frame.
         """
 
-        size = self.shape
+        height, width = self.shape
 
         depth_frame = np.expand_dims(depth_frame, axis=2)
 
-        downsample = 10
+        downsample = 5
 
-        mid_h = size[0]/2
-        mid_w = size[1]/2
+        mid_h = height/2
+        mid_w = width/2
 
-        pdf_w = self.pdf(size[1])
+        pdf_w = self.pdf(width)
         max = np.max(pdf_w)
         pdf_w = pdf_w / max
 
-        index_y = np.linspace(0, size[0]-1, size[0]//downsample, dtype=int)
-        index_x = np.linspace(0, size[1]-1, size[1]//downsample, dtype=int)
+        index_y = np.linspace(0, height-1, height//downsample, dtype=int)
+        index_x = np.linspace(0, width-1, width//downsample, dtype=int)
+
+        # Camera intrinsics
+        fovy = 60
+        c_y = height / 2
+        c_x = width / 2
+        f_y = 0.5 * height / math.tan(fovy * math.pi / 360)
+        f_x = f_y * (width / height)
 
         points = []
-
-        scale = size[1] / size[0]
 
         # Finding [x,y,z] coordinates of each pixel
         for i in index_y:
@@ -63,12 +69,11 @@ class PointCloud:
 
                 # Obtaining the data from
                 # mat = np.array([[1],
-                #                 [-(j - mid_w) * scale / size[1]],
-                #                 [-(i - mid_h) / size[0]]])
+                #                 [-(j - mid_w) * scale / width],
+                #                 [-(i - mid_h) / height]])
                 
-                mat = np.array([
-                                [-(i - mid_h) / size[0]],
-                                [-(j - mid_w) * scale / size[1]],
+                mat = np.array([[(i - c_y) / f_y],
+                                [(j - c_x) * (width/height) / f_x],
                                 [1]])
                 
                 # Saving the point in the world frame
@@ -76,15 +81,17 @@ class PointCloud:
 
                 # Rotating the point to the world frame with pivot at the camera
                 point = np.matmul(z_rot, -point)
-
+                
                 # Changis axis
                 transform = np.array([[0, 0, 1],
-                                      [0, 1, 0],
-                                      [-1, 0, 0]])
-                
+                                      [0, -1, 0],
+                                      [1, 0, 0]])
                 point = np.matmul(transform, point)
 
                 point = point + camera_pos
+
+                if point[2] < 0.03:
+                    continue
 
                 # Adding the point to the list of points
                 points.append(point)