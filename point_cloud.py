cb516b77695d7c12816620ed8ab12d7e3ac14df2
Eduardo AG <edag4000@gmail.com>
2023-09-14 13:09:51 -0400

    Adding addition camera to xml and optimization of point cloud estimator

--- point_cloud.py
+++ point_cloud.py
@@ -7,6 +7,18 @@ class PointCloud:
         self.shape = shape
 
 
+    
+    # A custom function to calculate
+    # probability distribution function
+    def pdf(self, size):
+        x = np.arange(0, size, 1)
+
+        mean = np.mean(x)
+        std = np.std(x) + 380
+        y_out = 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (x - mean)**2 / (2 * std**2))
+        return y_out
+
+
 
     def get_points(self, depth_frame, camera_pos, z_rot):
         """
@@ -21,46 +33,63 @@ class PointCloud:
 
         size = self.shape
 
+        depth_frame = np.expand_dims(depth_frame, axis=2)
+
         downsample = 10
 
         mid_h = size[0]/2
         mid_w = size[1]/2
+
+        pdf_w = self.pdf(size[1])
+        max = np.max(pdf_w)
+        pdf_w = pdf_w / max
 
         index_y = np.linspace(0, size[0]-1, size[0]//downsample, dtype=int)
         index_x = np.linspace(0, size[1]-1, size[1]//downsample, dtype=int)
 
         points = []
 
+        scale = size[1] / size[0]
+
         # Finding [x,y,z] coordinates of each pixel
-        for i in range(index_y.shape[0]-1):
-            for j in range(index_x.shape[0]-1):
+        for i in index_y:
+            for j in index_x:
 
-                i_l = index_y[i]
-                j_l = index_x[j]
+                D = depth_frame[i][j]
 
-                # Finding the y value based on trigonomety
-                D = depth_frame[i_l][j_l]
-
-                if np.isnan(D):
+                # Skip if the pixel is not in the image
+                if np.isnan(D[0]):
                     continue
 
                 # Obtaining the data from
-                x = D
-                y = -(j_l - mid_w) * x / size[1]
-                z = -(i_l - mid_h) * x / size[0]
-
+                # mat = np.array([[1],
+                #                 [-(j - mid_w) * scale / size[1]],
+                #                 [-(i - mid_h) / size[0]]])
+                
+                mat = np.array([
+                                [-(i - mid_h) / size[0]],
+                                [-(j - mid_w) * scale / size[1]],
+                                [1]])
+                
                 # Saving the point in the world frame
-                point = np.array([x, y, z])
+                point = np.matmul(mat, D)
 
                 # Rotating the point to the world frame with pivot at the camera
-                # z_rotation = np.array([[np.cos(z_rot), -np.sin(z_rot), 0],
-                #                        [np.sin(z_rot),  np.cos(z_rot), 0],
-                #                        [0,              0,             1]])
-                # point = np.matmul(z_rotation, point)
+                point = np.matmul(z_rot, -point)
+
+                # Changis axis
+                transform = np.array([[0, 0, 1],
+                                      [0, 1, 0],
+                                      [-1, 0, 0]])
+                
+                point = np.matmul(transform, point)
+
                 point = point + camera_pos
 
                 # Adding the point to the list of points
                 points.append(point)
+
+        points.append(camera_pos)
 
         return points
 
