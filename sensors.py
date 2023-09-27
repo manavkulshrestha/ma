b9723d21141e2576749eea10eecfbeff255ec1a2
Eduardo AG <edag4000@gmail.com>
2023-09-27 16:50:20 -0400

    Duplicating functions for fine data collection

--- sensors.py
+++ sensors.py
@@ -5,6 +5,70 @@ class Sensors:
 
     def __init__(self) -> None:
         pass
+
+    def get_depth_image_matrices(self,
+                                  m:mujoco.MjModel,
+                                  d:mujoco.MjData,
+                                  r:mujoco.Renderer,
+                                  cameras=None|list[str]|list[int]) -> dict[any, np.ndarray]:
+        """
+        Returns dictionary of RGB image matrices for each camera in the model.
+
+        Parameters
+        ----------
+        m : The mujoco model.
+        d : The mujoco data.
+        r : The mujoco renderer.
+        cameras : The list of cameras to render. If None, all cameras are rendered. The default is None.
+        """
+
+        result = {}
+
+        if type(cameras) == list:
+            cams = cameras
+        else:
+            cams = range(m.cam_user.shape[0])
+
+        for cam in cams:
+                r.update_scene(d, cam)
+                # Can add out = np.zeros((m.vis.map[cam].height, m.vis.map[cam].width, 3), 
+                # dtype=np.uint8) to render() to save memory
+                render = r.render()
+                result[cam] = render
+                
+        return result
+    
+    def get_segment_image_matrices(self,
+                                  m:mujoco.MjModel,
+                                  d:mujoco.MjData,
+                                  r:mujoco.Renderer,
+                                  cameras=None|list[str]|list[int]) -> dict[any, np.ndarray]:
+        """
+        Returns dictionary of RGB image matrices for each camera in the model.
+
+        Parameters
+        ----------
+        m : The mujoco model.
+        d : The mujoco data.
+        r : The mujoco renderer.
+        cameras : The list of cameras to render. If None, all cameras are rendered. The default is None.
+        """
+
+        result = {}
+
+        if type(cameras) == list:
+            cams = cameras
+        else:
+            cams = range(m.cam_user.shape[0])
+
+        for cam in cams:
+                r.update_scene(d, cam)
+                # Can add out = np.zeros((m.vis.map[cam].height, m.vis.map[cam].width, 3), 
+                # dtype=np.uint8) to render() to save memory
+                render = r.render()[:, :, 0]
+                result[cam] = render
+                
+        return result
     
     def get_rgb_image_matrices(self,
                                m:mujoco.MjModel,
