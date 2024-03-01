b96b938ad04d83b0a8c06ca94ff83875b079b252
layla <edag4000@gmail.com>
2024-03-01 11:55:39 -0600

    Update

--- scripts/utils/leg_controller.py
+++ scripts/utils/leg_controller.py
@@ -7,12 +7,43 @@ import numpy as np
 
 class LegController:
 
-    def __init__(self, l1, l2, l3, theta):
+    def __init__(self, l1, l2, l3, phi):
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
-        self.theta = theta
+        self.phi = phi
 
-    def ctrl(self) -> np.ndarray:
+    def atan(z, x):
+        if (x > 0 and z >= 0):
+            return np.arctan(z/x)
+        elif (x == 0 and z >= 0):
+            return np.pi/2
+        elif (x < 0 and z >= 0):
+            return -abs(np.arctan(z/x) + np.pi)
+        elif (x < 0 and z < 0):
+            return np.arctan(z/x) + np.pi
+        elif (x > 0 and ):
+            
+        elif (x == 0 and z < 0):
+            return -np.pi/2
+        else:
+            return 0
+
+    def ctrl(self, goal, alpha1) -> np.ndarray:
         # Control the leg
+
+        # Linear distance from fromt (origin to goal)
+        a = np.linalg.norm(np.array([goal[0], goal[1]]))
+        alpha2 = np.arcsin(self.l1/a * np.sin(self.phi))
+        alpha3 = np.pi - alpha2 - self.phi
+        theta1 = alpha1 - alpha3
+
+        # Calculate the position of the end effector
+        r = theta1 + self.phi - np.pi/2
+
+        b = np.linalg.norm(np.array([goal[0], goal[2]]))
+        beta1 = np.arctan(goal[2]/goal[0])
+
+
         return np.array([0, 0, -0.888])
+    
--- scripts/utils/sim.py
+++ scripts/utils/sim.py
@@ -1,10 +1,10 @@
+import numpy as np
 import mujoco as mj
 import mujoco.viewer
 import time
 
 # Utility class to run a simulation
 # Runs a mujoco xml and allows to control it
-
 class Sim:
 
     def __init__(self,
@@ -28,41 +28,35 @@ class Sim:
         self.d = mj.MjData(self.m)
 
         # Setting up the initial value of the joints
-        self.d.ctrl = [0, 0, -0.888,
-                       0, 0, -0.888,
-                       0, 0, -0.888,
-                       0, 0, -0.888,]
+        self.d.ctrl = [0, 1.21, -2.78,
+                       0, 1.21, -2.78,
+                       0, 1.21, -2.78,
+                       0, 1.21, -2.78,]
 
         self.step = 0
         self.switch = True
 
     def start_debug(self):
-        print(f"Actuator: {self.d.act}")
         print(f"Control: {self.d.ctrl}")
+        print(f"Type: {type(self.d.ctrl)}")
+        print(f"Single leg: {self.d.ctrl[0:3]}")
 
     def run_debug(self):
         if self.step % 100 == 0:
             self.start_debug()
-            self.switch = not self.switch
-            self.step += 1
-        else:
-            self.step += 1
-
-        self.ctrl()
 
     # Abstract class to control the simulation
     def ctrl(self):
-        pass
-        # if self.switch:
-        #     self.d.ctrl = [0, 0, -0.888,
-        #                    0, 0, -0.888,
-        #                    0, 0, -0.888,
-        #                    0, 0, -0.888,]
-        # else:
-        #     self.d.ctrl = [0, 0, -2,
-        #                    0, 0, -2,
-        #                    0, 0, -2,
-        #                    0, 0, -2,]
+        if self.step == 200:
+            single_leg_angles = self.d.ctrl[0:3]
+            print(f"Single leg angles: {single_leg_angles}")
+
+            self.d.ctrl = np.concatenate((single_leg_angles, 
+                                          single_leg_angles, 
+                                          single_leg_angles, 
+                                          single_leg_angles),
+                                          axis=None)
+            self.step = 1
 
     def view_sim(self, debug=False):
         # Debugging
@@ -72,13 +66,14 @@ class Sim:
         with mujoco.viewer.launch_passive(self.m, self.d) as viewer:
             while viewer.is_running():
                 # Call the control function
-                # self.ctrl()
+                self.ctrl()
 
                 # Debugging
                 if debug:
                     self.run_debug()
 
                 # Step the simulation
+                self.step += 1
                 mujoco.mj_step(self.m, self.d)
                 viewer.sync()
                 time.sleep(0.01)
